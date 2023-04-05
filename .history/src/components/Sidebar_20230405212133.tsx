624a7b56e2653f3042f2441613c120f930135fcb
東航平 <higashikohei@azumakouheinoMacBook-Air.local>
2023-04-05 21:29:34 +0900

    [Add]sidebar追加

---
+++ .history/src/components/Sidebar_20230405212133.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+      {/* sidevbarLeft */}
+      {/* sidebarRight */}
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+      {/* sidevbarLeft */}
+      <div className="sidebarLeft">
+        <div className="serverIcon">
+            <img src="./logo192.png" alt="" />
+        </div>
+        <div className="serverIcon">
+            <img src="./logo192.png" alt="" />
+        </div>
+      </div>
+      {/* sidebarRight */}
+      <div className="sidebarRight"></div>
+    </div>
+  )
+}
+
+export default Sidebar
